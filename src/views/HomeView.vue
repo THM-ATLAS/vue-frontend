e8d7ec5542ba46284a916158853926bec632b78c
Vanessa Nagy <vanessa.nagy@mni.thm.de>
2022-04-26 17:03:34 +0200

    Vuetify basic project

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="home">
+    <v-img alt="Vue logo" width="100px" style="display: block;
+  margin-left: auto;
+  margin-right: auto" src="../assets/logo.png"></v-img>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
