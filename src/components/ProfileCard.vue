b678415b4fe9936068de3131f654eaa31388ae37
Maxim <maximoweller@gmail.com>
2022-05-06 13:41:59 +0200

    Profile card is now using script setup correctly

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -2,44 +2,44 @@
   <br />
   <v-card class="profile-card" rounded="0" elevation="0">
     <v-container>
-        <v-row class="profile-header" align="center" no-gutters>
-          <v-col cols="12" md="3">
-            <v-avatar :image="profile.image" size="160"></v-avatar
-          ></v-col>
-          <v-col cols="12" md="5">
-            <h2>{{ profile.fullName }}</h2>
-            <div class="profile-username">{{ profile.username }}</div></v-col
-          >
-          <v-col cols="12" md="3">
-            <!-- HIER SPÄTER BADGES? -->
-          </v-col>
-          <v-col cols="12" md="1">
-            <v-btn class="edit-button" icon variant="outlined">
-              <v-icon icon="mdi-brush" />
-            </v-btn>
-          </v-col>
-        </v-row>
+      <v-row class="profile-header" align="center" no-gutters>
+        <v-col cols="12" md="3">
+          <v-avatar :image="profile.image" size="160"></v-avatar
+        ></v-col>
+        <v-col cols="12" md="5">
+          <h2>{{ profile.fullName }}</h2>
+          <div class="profile-username">{{ profile.username }}</div></v-col
+        >
+        <v-col cols="12" md="3">
+          <!-- HIER SPÄTER BADGES? -->
+        </v-col>
+        <v-col cols="12" md="1">
+          <v-btn class="edit-button" icon variant="outlined">
+            <v-icon icon="mdi-brush" />
+          </v-btn>
+        </v-col>
+      </v-row>
 
-        <v-row class="profile-stats text-center">
-          <v-col>
-            <v-card variant="outlined">
-              <div><b>Gelöste Aufgaben</b></div>
-              <div>{{ profile.stats.finished }}</div>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card variant="outlined">
-              <div><b>In ATLAS verbrachte Zeit</b></div>
-              <div>{{ profile.stats.time }}</div>
-            </v-card>
-          </v-col>
-          <v-col>
-            <v-card variant="outlined">
-              <div><b>Beigetreten am</b></div>
-              <div>{{ profile.stats.joined }}</div>
-            </v-card>
-          </v-col>
-        </v-row>
+      <v-row class="profile-stats text-center">
+        <v-col>
+          <v-card variant="outlined">
+            <div><b>Gelöste Aufgaben</b></div>
+            <div>{{ profile.stats.finished }}</div>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card variant="outlined">
+            <div><b>In ATLAS verbrachte Zeit</b></div>
+            <div>{{ profile.stats.time }}</div>
+          </v-card>
+        </v-col>
+        <v-col>
+          <v-card variant="outlined">
+            <div><b>Beigetreten am</b></div>
+            <div>{{ profile.stats.joined }}</div>
+          </v-card>
+        </v-col>
+      </v-row>
 
       <v-row>
         <v-col cols="12" class="center-text">
@@ -66,52 +66,42 @@
   <br />
 </template>
 
-<script>
+<script setup>
 import { useRoute, useRouter } from "vue-router";
 import { computed } from "vue";
 
-export default {
-  name: "ProfileCard",
-  components: {},
-  setup() {
-    const router = useRouter();
+const router = useRouter();
 
-    const getID = computed(() => {
-      if (!useRoute().params.id) {
-        return undefined;
-      }
-      return useRoute().params.id instanceof Array
-        ? useRoute().params.id[0]
-        : useRoute().params.id;
-    });
+const getID = computed(() => {
+  if (!useRoute().params.id) {
+    return undefined;
+  }
+  return useRoute().params.id instanceof Array
+    ? useRoute().params.id[0]
+    : useRoute().params.id;
+});
 
-    const id = getID.value === undefined ? "abcd66" : getID.value; // at 1 would be a call to get own profile id
+const id = getID.value === undefined ? "abcd66" : getID.value; // at 1 would be a call to get own profile id
 
-    console.log(id);
+console.log(id);
 
-    if (getID.value === undefined) {
-      router.replace(`/u/${id}`);
-    }
+if (getID.value === undefined) {
+  router.replace(`/u/${id}`);
+}
 
-    //Profildaten kommen auf lange Sicht aus der Datenbank und nicht hier aus den Daten, hier nur Platzhalter weil kein Backend
-    const profile = {
-      username: id,
-      fullName: "Marianne Musterfrau",
-      image: require("../assets/marianneMuster.png"),
-      marked: ["Aufgabe 1", "Aufgabe 2", "Aufgabe 3"],
-      level: 5,
-      xp: 25,
-      stats: {
-        finished: 9,
-        time: "20:42",
-        rating: 3.9,
-        joined: "01.01.2022",
-      },
-    };
-
-    return {
-      profile,
-    };
+//Profildaten kommen auf lange Sicht aus der Datenbank und nicht hier aus den Daten, hier nur Platzhalter weil kein Backend
+const profile = {
+  username: id,
+  fullName: "Marianne Musterfrau",
+  image: require("../assets/marianneMuster.png"),
+  marked: ["Aufgabe 1", "Aufgabe 2", "Aufgabe 3"],
+  level: 5,
+  xp: 25,
+  stats: {
+    finished: 9,
+    time: "20:42",
+    rating: 3.9,
+    joined: "01.01.2022",
   },
 };
 </script>
