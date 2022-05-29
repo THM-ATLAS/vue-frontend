49c629f168ebb12cefd8dc362de8d1c9139ad374
shinymew2 <maxim.odenweller@mni.thm.de>
2022-05-30 00:34:27 +0200

    Change layout

--- src/components/ModuleManager.vue
+++ src/components/ModuleManager.vue
@@ -9,7 +9,7 @@
           <v-card-title>Modulname</v-card-title>
         </v-col>
         <v-col cols="2" align-self="center">
-          <v-btn>Edit Tags</v-btn>
+          <v-btn @click="manageTagsDialog.show = true">Edit Tags </v-btn>
         </v-col>
       </v-row>
     </v-card>
@@ -25,53 +25,9 @@
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>Max Mustermann</td>
-                <td>Admin, Student</td>
-              </tr>
-              <tr>
-                <td>Test Tester</td>
-                <td>Student</td>
-              </tr>
-              <tr>
-                <td>Marie Musterfrau</td>
-                <td>Tutor, Student</td>
-              </tr>
-              <tr>
-                <td>Max Mustermann</td>
-                <td>Admin, Student</td>
-              </tr>
-              <tr>
-                <td>Test Tester</td>
-                <td>Student</td>
-              </tr>
-              <tr>
-                <td>Marie Musterfrau</td>
-                <td>Tutor, Student</td>
-              </tr>
-              <tr>
-                <td>Max Mustermann</td>
-                <td>Admin, Student</td>
-              </tr>
-              <tr>
-                <td>Test Tester</td>
-                <td>Student</td>
-              </tr>
-              <tr>
-                <td>Marie Musterfrau</td>
-                <td>Tutor, Student</td>
-              </tr>
-              <tr>
-                <td>Max Mustermann</td>
-                <td>Admin, Student</td>
-              </tr>
-              <tr>
-                <td>Test Tester</td>
-                <td>Student</td>
-              </tr>
-              <tr>
-                <td>Marie Musterfrau</td>
-                <td>Tutor, Student</td>
+              <tr v-for="user in users" v-bind:key="user.user_id">
+                <td>{{ user.name }}</td>
+                <td>Rollen anzeigen</td>
               </tr>
             </tbody>
           </v-table>
@@ -79,17 +35,60 @@
         <v-col cols="12">
           <v-pagination></v-pagination>
         </v-col>
-      </v-row> </v-card
-  ></v-container>
+      </v-row>
+    </v-card>
+
+    <!-- Edit tags dialog start -->
+    <v-dialog v-model="manageTagsDialog.show" :retain-focus="false" transition="slide-y-transition">
+      <v-card top="20%" width="50vw">
+        <v-card-title> Tags für Modulname </v-card-title>
+        <v-card-text>
+          TABELLE MIT TAGS ZUM SELECTEN
+        </v-card-text>
+        <v-card-actions>
+          <v-btn
+            @click="manageTagsDialog.show = false"
+            color="error"
+            v-html="$t('buttons.cancel')"
+          />
+          <v-btn
+            @click="manageTagsDialog.show = false"
+            color="primary"
+            v-html="$t('buttons.save')"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <!-- Edit tags dialog end -->
+  </v-container>
 </template>
 
 <script setup lang="ts">
+import { onBeforeMount, Ref, ref } from "vue";
 import { useRouter } from "vue-router";
+import UserService from "@/services/UserService";
+import { User } from "@/helpers/types";
+
 const router = useRouter();
+const users: Ref<User[]> = ref([]);
+
+async function loadUsers(): Promise<void> {
+  users.value = (await UserService.getUsers()).data.sort(
+    (a, b) => a.user_id - b.user_id
+  );
+}
+
+onBeforeMount(async () => {
+  await loadUsers();
+});
 
 function goBack(): void {
   router.back();
 }
+
+const manageTagsDialog = ref({
+  show: false,
+});
 </script>
 
 <style scoped></style>
