4a3fee1510432d29e778489a645d0bc215dc0933
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2022-05-30 12:34:02 +0200

    update md-editor, use github theme

--- src/components/helpers/MarkdownModal.vue
+++ src/components/helpers/MarkdownModal.vue
@@ -1,6 +1,8 @@
 <template>
   <Editor
       v-if="editor"
+      preview-theme="github"
+      code-theme="github"
       :theme="theme"
       v-model="contentRef"
       language="en-US"
@@ -12,9 +14,11 @@
   <Editor
       v-else
       style="background: none"
+      preview-theme="github"
+      code-theme="github"
       previewOnly
       :theme="theme"
-      class="text-left"
+      class="text-block hyphenate"
       language="en-US"
       :modelValue="modelValue"
       :showCodeRowNumber="true"/>
@@ -49,5 +53,12 @@ const toolbars = [
 </script>
 
 <style scoped>
+.hyphenate {
+  word-wrap: break-word;
+  overflow-wrap: break-word;
 
+  -webkit-hyphens: auto;
+  -moz-hyphens: auto;
+  hyphens: auto;
+}
 </style>
