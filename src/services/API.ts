5803622d4f60ced1c7f0015b235e66a151dfc76e
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2022-06-02 20:11:01 +0200

    use dev api (can be toggled)

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,7 +1,9 @@
 import axios, {AxiosInstance} from "axios";
 
+const isDev = true; // TODO: change to false when in prod
+
 const API: AxiosInstance = axios.create({
-    baseURL: "http://brueckenkurs-programmieren.thm.de/api/",
+    baseURL: `http://brueckenkurs-programmieren.thm.de/api${isDev ? '-dev' : ''}/`,
     headers: {
         "Content-type": "application/json",
     },
