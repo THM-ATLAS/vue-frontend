e52cbf9e0c5c985fc3fcd69aa1834914d05d4fe4
shinymew2 <maxim.odenweller@mni.thm.de>
2022-06-06 20:51:13 +0200

    Add and delete for module users working, edited the layout of the module manager and placed a placeholder button at the ModuleMainPage for future usage

--- src/components/ModuleManager.vue
+++ src/components/ModuleManager.vue
@@ -1,11 +1,6 @@
 <template>
   <v-container>
     <v-card>
-      <!-- TEST BUTTON -->
-      <v-btn @click="addMaxim()">Add me</v-btn>
-      <v-btn @click="removeMaxim()">Remove me</v-btn>
-      <v-btn @click="logButton()">Log</v-btn>
-      <!-- TEST BUTTON -->
       <v-row>
         <v-col cols="1" align-self="center">
           <v-btn @click="goBack()" class="back-button" rounded="false">
@@ -31,12 +26,27 @@
               <tr>
                 <th class="text-left">{{ $t("module_manager.name") }}</th>
                 <th class="text-left">{{ $t("module_manager.roles") }}</th>
+                <th class="text-right"></th>
+                <th class="text-right"></th>
               </tr>
             </thead>
             <tbody>
               <tr v-for="user in moduleUsers" v-bind:key="user.user_id">
-                <td>{{ user.name }}</td>
-                <td>{{ user.module_role.name }}</td>
+                <td class="text-left">{{ user.name }}</td>
+                <td class="text-left">{{ user.module_role.name }}</td>
+                <td class="placeholder-td"></td>
+                <td class="text-right">
+                  <v-btn class="manage-button" color="primary">
+                    <v-icon icon="mdi-cog"></v-icon>
+                  </v-btn>
+                  <v-btn
+                    class="manage-button"
+                    @click="deleteModuleUser(user)"
+                    color="error"
+                  >
+                    <v-icon icon="mdi-delete"></v-icon>
+                  </v-btn>
+                </td>
               </tr>
             </tbody>
           </v-table>
@@ -55,8 +65,7 @@
       transition="slide-y-transition"
     >
       <v-card top="20%" width="50vw">
-        <v-card-title> Tags für Modulname </v-card-title>
-        <v-card-text> TABELLE MIT TAGS ZUM SELECTEN </v-card-text>
+        <v-card-title> WIP </v-card-title>
         <v-card-actions>
           <v-btn
             @click="manageTagsDialog.show = false"
@@ -81,7 +90,7 @@
     >
       <v-card top="20%" width="50vw">
         <v-card-title> User hinzufügen </v-card-title>
-        <v-table>
+        <v-table fixed-header height="400px">
           <thead>
             <tr>
               <th>Name</th>
@@ -89,22 +98,24 @@
             </tr>
           </thead>
           <tbody>
-              <tr v-for="user in users" v-bind:key="user.user_id">
-                <td>{{ user.name }}</td>
-                <td class="text-right">
-                  <v-btn
-                  @click="logger(user.user_id)">
-                    Hinzufügen
-                  </v-btn>
-                </td>
-              </tr>
+            <tr v-for="user in filteredUsers" v-bind:key="user.user_id">
+              <td v-if="user.name">{{ user.name }}</td>
+              <td v-else>No users to add</td>
+              <td class="text-right">
+                <v-btn 
+                @click="addModuleUser(user.user_id)"
+                color="primary"
+                >
+                  <v-icon icon="mdi-account-plus"></v-icon>
+                </v-btn>
+              </td>
+            </tr>
           </tbody>
         </v-table>
         <v-card-actions>
-          <v-btn
-            @click="manageUsersDialog.show = false"
-            color="error"
-          >Schließen</v-btn>
+          <v-btn @click="manageUsersDialog.show = false" color="error"
+            >Schließen</v-btn
+          >
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -113,35 +124,20 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount, Ref, ref } from "vue";
+import { onBeforeMount, Ref, ref,  } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import UserService from "@/services/UserService";
 import ModuleService from "@/services/ModuleService";
 import ModuleManagerService from "@/services/ModuleManagerService";
 import { ModuleUser, User, Module } from "@/helpers/types";
 
+//Router
 const route = useRoute();
-
 const router = useRouter();
-const users: Ref<User[]> = ref([]);
-const moduleUsers: Ref<ModuleUser[]> = ref([]);
 const module: Ref<Module> = ref({}) as Ref<Module>;
-const user = {
-  user_id: 5,
-  module_role: {
-    role_id: 3,
-    name: "student",
-  },
-  name: "Maxim Odenweller",
-  username: "modn04",
-  email: "maxim.odenweller@mni.thm.de",
-};
-
-async function loadUsers(): Promise<void> {
-  users.value = (await UserService.getUsers()).data.sort(
-    (a, b) => a.user_id - b.user_id
-  );
-}
+const moduleUsers: Ref<ModuleUser[]> = ref([]);
+const allUsers: Ref<User[]> = ref([]);
+const filteredUsers: Ref<User[]> = ref([]);
 
 async function loadModule(): Promise<void> {
   ModuleService.getModule(route.params.module).then((res) => {
@@ -155,11 +151,59 @@ async function loadModuleUsers(): Promise<void> {
   ).data.sort((a, b) => a.user_id - b.user_id);
 }
 
+async function loadAllUsers(): Promise<void> {
+  allUsers.value = (await UserService.getUsers()).data.sort(
+    (a, b) => a.user_id - b.user_id
+  );
+}
+
+async function loadFilteredUsers(): Promise<void> {
+  filteredUsers.value = await allUsers.value
+    .map(({ user_id, name }) => ({ user_id, name }))
+    .filter(
+      (a) => !moduleUsers.value.map((a) => a.user_id).includes(a.user_id)
+    );
+}
+
 onBeforeMount(async () => {
   await loadModule();
-  await loadUsers();
+  await loadAllUsers();
   await loadModuleUsers();
+  await loadFilteredUsers();
 });
+
+function addModuleUser(number: Number): void {
+  const user = ref({});
+  const moduleUser = ref({});
+  user.value = allUsers.value.filter((a) => a.user_id == number)[0];
+  moduleUser.value = getUserTemplate();
+  moduleUser.value.user_id = user.value.user_id;
+  moduleUser.value.name = user.value.name;
+  moduleUser.value.username = user.value.username;
+  moduleUser.value.email = user.value.email;
+  ModuleManagerService.addModuleUser(module.value, moduleUser.value)
+    .then(() => loadModuleUsers())
+    .then(() => loadFilteredUsers());
+}
+
+function deleteModuleUser(user: ModuleUser): void {
+  ModuleManagerService.delModuleUser(module.value, user)
+    .then(() => loadModuleUsers())
+    .then(() => loadFilteredUsers());
+}
+
+function getUserTemplate(): ModuleUser {
+  return {
+    user_id: 0,
+    module_role: {
+      role_id: 5,
+      name: "tutor",
+    },
+    name: "",
+    username: "",
+    email: "",
+  };
+}
 
 function goBack(): void {
   router.back();
@@ -172,32 +216,10 @@ const manageTagsDialog = ref({
 const manageUsersDialog = ref({
   show: false,
 });
-
-/**
- * Test stuff
- */
-
-function addMaxim(): void {
-  ModuleManagerService.addModuleUser(module.value, user).then(() => loadModuleUsers());
-}
-
-function removeMaxim(): void {
-  ModuleManagerService.delModuleUser(module.value, user).then(() => loadModuleUsers());
-}
-
-function logButton(): void {
-  console.log(users.value.map(({user_id, name}) => ({user_id, name})));
-  console.log(moduleUsers.value.map(({user_id, name}) => ({user_id, name})));
-  //console.log(users.value.map(({user_id, name}) => ({user_id, name})).filter((user) => !moduleUsers.value.map(({user_id, name}) => ({user_id, name})).includes(user)));
-}
-
-function logger(number: Number): void {
-  console.log(number);
-}
 </script>
 
 <style scoped>
-.text-right {
-  text-align: right;
+.manage-button {
+  margin-right: 1em;
 }
 </style>
--- src/services/ModuleManagerService.ts
+++ src/services/ModuleManagerService.ts
@@ -11,8 +11,20 @@ class ModuleManagerService {
         return API.post(`modules/users/${module.module_id}`, user);
     }
 
+    editModuleUser(module: Module, user: ModuleUser): Promise<any> {
+        return API.put(`modules/users/${module.module_id}`, user)
+    }
+
     delModuleUser(module: Module, user: ModuleUser): Promise<any> {
         return API.delete(`modules/users/${module.module_id}/${user.user_id}`);
+    }
+
+    addMultipleModuleUsers(module: Module, users: ModuleUser[]) {
+        return API.post(`modules/users/multiple/${module.module_id}`, users);
+    }
+
+    delMultipleModuleUsers(module: Module, users: ModuleUser[]) {
+        return API.post(`modules/users/${module.module_id}`, users);
     }
 }
 
