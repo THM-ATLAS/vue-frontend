fce868eb90f47b32cdca58ee72edb3c0f58405a7
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2022-06-13 20:45:48 +0200

    hard-code axios api url for main

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,10 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
-const isDev = false; // TODO: change to false when in prod
-const isLocalHost = false;
-
 const API: AxiosInstance = axios.create({
-    baseURL: isLocalHost ? `http://localhost:8080/` : `http://brueckenkurs-programmieren.thm.de/api${isDev ? '-dev' : ''}/`,
+    baseURL: `http://brueckenkurs-programmieren.thm.de/api/`,
     headers: {
         "Content-type": "application/json",
     },
