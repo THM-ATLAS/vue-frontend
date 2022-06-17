d5963d66628147ddc4a1773c7605369098508c97
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2022-06-17 14:23:28 +0200

    use localhost without api for auth

--- src/services/API.ts
+++ src/services/API.ts
@@ -4,11 +4,20 @@ const isDev = true;
 const isLocalHost = true;
 */
 
-const API: AxiosInstance = axios.create({
+export const API: AxiosInstance = axios.create({
     baseURL: `http://localhost:8080/api/`,
     headers: {
         "Content-type": "application/json",
     },
     withCredentials: true,
 });
+
+export const AuthAPI: AxiosInstance = axios.create({ // only used for login and logout
+    baseURL: `http://localhost:8080/`,
+    headers: {
+        "Content-type": "application/json",
+    },
+    withCredentials: true,
+});
+
 export default API;
