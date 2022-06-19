75940128de9083310efa68d0c99ef979aa924819
rene <rene.gentzen@mni.thm.de>
2022-06-19 14:31:05 +0200

    Re-enabled refreshing of page title when changing language

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -25,7 +25,7 @@
 import Navbar from "@/components/navigation/navbar/NavbarBase.vue";
 import Settings from "@/components/SettingsCard.vue";
 import Footer from "@/components/navigation/FooterCard.vue";
-/*import {useI18n} from "vue-i18n";
+import {useI18n} from "vue-i18n";
 import {watch, ref} from "vue";
 
 const i18n = useI18n()
@@ -35,5 +35,5 @@ const currentLocale = ref(i18n.locale)
 watch(currentLocale, () => {
   document.title = i18n.t('titles.settings_view');
 })
-document.title = i18n.t('titles.settings_view');*/
+document.title = i18n.t('titles.settings_view');
 </script>
