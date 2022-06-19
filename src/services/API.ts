3e6b7585e55c6dc4cb652216f32c517f05ae8fe5
rene <rene.gentzen@mni.thm.de>
2022-06-19 13:38:29 +0200

    Fixed small issue in docker-compose file and aligned api base link with newest specification

--- src/services/API.ts
+++ src/services/API.ts
@@ -4,7 +4,7 @@ const isDev = true; // TODO: change to false when in prod
 const isLocalHost = true;
 
 const API: AxiosInstance = axios.create({
-    baseURL: isLocalHost ? `http://localhost:8080/` : `http://brueckenkurs-programmieren.thm.de/api${isDev ? '-dev' : ''}/`,
+    baseURL: isLocalHost ? `http://localhost:8080/api/` : `http://brueckenkurs-programmieren.thm.de/api${isDev ? '-dev' : ''}/`,
     headers: {
         "Content-type": "application/json",
     },
