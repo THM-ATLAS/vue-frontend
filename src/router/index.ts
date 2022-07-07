9da4a1c5d39d06fea02ebe05f4c21b43bac47ea2
Hendrik Wagner <hendrik.wagner@mni.thm.de>
2022-07-07 19:23:53 +0200

    Restructure router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+import {createRouter, createWebHistory, RouteRecordRaw} from 'vue-router'
 import Login from "@/views/misc/LoginView.vue";
 import Exercise from "@/views/exercises/ExerciseView.vue";
 
@@ -9,7 +9,6 @@ import SpecificEvaluation from "@/views/exercises/submissions/SpecificEvaluation
 import EvaluationList from "@/views/exercises/submissions/EvaluationListView.vue";
 import Notifications from "@/views/user/NotificationsView.vue";
 import Help from "@/views/misc/HelpView.vue";
-import Leaderboard from "@/views/user/LeaderboardView.vue";
 import ExerciseEditor from "@/views/exercises/ExerciseEditorView.vue";
 import ExerciseCreator from "@/views/exercises/ExerciseCreatorView.vue";
 import Feedback from "@/views/exercises/feedback/FeedbackView.vue";
@@ -31,65 +30,83 @@ import {isLoggedIn} from "@/services/LoginService";
 import {AxiosResponse} from "axios";
 
 const routes: Array<RouteRecordRaw> = [
-  {path: '/', name: 'Home',component: Home},
+    {path: '/', name: 'Home', component: Home},
 
-  {path: '/login', component: Login},
-  { path: '/page-not-found', name: 'NotFound', component: PageNotFound },
-  { path: '/:pathMatch(.*)*', redirect: '/page-not-found' },
+    {path: '/login', component: Login},
+    {path: '/notifications', component: Notifications},
+    {path: '/help', component: Help},
+    {path: '/settings', component: Settings},
+    {path: '/modules', component: ModuleSearch},
 
-  {path: '/:module/', component: ModuleMainPage},
-  {path: '/:module/manage', component: ModuleManager},
-  {path: '/:module/e/new/edit', component: ExerciseEditor}, // create new exercise
-  {path: '/:module/e/:id', component: Exercise},
-  {path: '/:module/e/:id/edit', component: ExerciseEditor}, // edit mode
-  {path: '/:module/e/new', component: ExerciseCreator}, // edit mode
-  {path: '/:module/e/:id/feedback', component: Feedback}, // list feedback
-  {path: '/:module/e/:id/:title', component: Exercise}, // title specified, internally ignored
+    {path: '/u/:id?', component: Profile},
 
-  {path: '/:module/s/:id', component: Submission},
-  {path: '/:module/s/:id/edit/:sid', component: EditSubmission},
-  {path: '/:module/eval/:id', component: EvaluationList},
-  {path: '/:module/eval/:id/:sid', component: SpecificEvaluation},
+    {
+        path: '/:module', children: [
+            {path: '', component: ModuleMainPage},
+            {path: 'manage', component: ModuleManager},
+            {
+                path: 'e', children: [
+                    {path: 'new', component: ExerciseCreator},
+                    {
+                        path: ':id', children: [
+                            {path: ':title?', component: Exercise},
+                            {path: 'edit', component: ExerciseEditor},
+                            {path: 'feedback', component: Feedback},
+                            {path: 'submissions', component: Submission},
+                        ]
+                    },
+                ]
+            },
+            {
+                path: 's/:id', children: [
+                    {path: '', component: Submission},
+                    {path: 'edit/:sid', component: EditSubmission},
+                    {path: 'evaluation/:sid', component: SpecificEvaluation},
+                ]
+            },
+            {
+                path: 'eval/:id', children: [
+                    {path: '', component: EvaluationList},
+                    {path: ':sid', component: SpecificEvaluation},
+                ]
+            },
+        ]
+    },
 
-  {path: '/u/', component: Profile}, // personal profile
-  {path: '/u/:id', component: Profile}, // profile of other user
+    {
+        path: '/admin', children: [
+            {path: '', component: Admin},
+            {path: 'users', component: UserManagement},
+            {path: 'exercises', component: ExerciseManagement},
+            {path: 'modules', component: ModuleManagement},
+        ]
+    },
 
-  {path: "/leaderboard", component: Leaderboard},
-
-  {path: '/notifications', component: Notifications},
-  {path: '/help', component: Help},
-
-  {path: '/settings', component: Settings},
-
-  {path: '/admin/', component: Admin},
-  {path: '/admin/users', component: UserManagement},
-  {path: '/admin/exercises', component: ExerciseManagement},
-  {path: '/admin/modules', component: ModuleManagement},
-
-  {path: '/modules', component: ModuleSearch},
+    {path: '/page-not-found', name: 'NotFound', component: PageNotFound},
+    {path: '/:pathMatch(.*)*', redirect: '/page-not-found'},
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-  scrollBehavior(to, from, savedPosition) {
-    return {
-      top: 0,
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+    scrollBehavior(to, from, savedPosition) {
+        return {
+            top: 0,
+        }
     }
-  }
 })
 
 router.beforeEach(async (to, from) => {
-    await UserService.getMe().then(async (r: AxiosResponse) =>{
-      if (isLoggedIn(r)) {
-        window.localStorage.setItem('loggedIn', 'true')
-        await SettingsService.getUserSettings(r.data.user_id).then( res => {
-          theme.value = res.data.theme
-          i18n.global.locale = res.data.language
-        })
-      } else {
-        window.localStorage.removeItem('loggedIn')
-      }
+    await UserService.getMe().then(async (r: AxiosResponse) => {
+        if (isLoggedIn(r)) {
+            window.localStorage.setItem('loggedIn', 'true')
+            await SettingsService.getUserSettings(r.data.user_id).then(res => {
+                theme.value = res.data.theme
+                i18n.global.locale = res.data.language
+            })
+        } else {
+            window.localStorage.removeItem('loggedIn')
+        }
     })
 })
 
