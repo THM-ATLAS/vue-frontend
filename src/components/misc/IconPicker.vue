416c6451feaaf673dbe35e634b9d0c2b47956740
daniel <poock.daniel.5+tea@gmail.com>
2022-07-11 13:43:31 +0200

    created icon picker

---
+++ src/components/misc/IconPicker.vue
@@ -0,0 +1,23 @@
+<template>
+<v-card>
+  <v-autocomplete v-model="icon">
+    
+  </v-autocomplete>
+  <p>{{icon}}</p>
+</v-card>
+</template>
+
+<script setup lang="ts">
+import {ref} from "vue";
+const iconNames =
+    ["android","apple","apple-ios",
+      "angular", "angularjs", "ansible", "application", "application-braces", "application-array",
+      "antenna", "artstation", "atom", "bacteria", "beaker", "blender-software",
+      "book", "bootstrap", "briefcase", "calculator", "calendar",
+      "camera", "cellphone", "chart-line", "cloud", "compass"]
+const icon = ref();
+</script>
+
+<style scoped lang="scss">
+
+</style>
