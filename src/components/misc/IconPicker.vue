69be06cf3ea1122bff16b6b6af349647ba745954
shinymew2 <maxim.odenweller@mni.thm.com>
2022-07-12 05:09:46 -0800

    made picking icon possible

--- src/components/misc/IconPicker.vue
+++ src/components/misc/IconPicker.vue
@@ -1,64 +1,32 @@
 <template>
-<v-card>
+      <v-card style="background: whitesmoke">
   <v-card-title>{{$t("icon_picker.title")}}</v-card-title>
-  <v-combobox
-      v-model="icon"
-      :items="iconNames"
-      persistent-hint
-      prepend-icon="mdi-magnify"
-  >
-    <template v-slot:append-outer>
-      <v-slide-x-reverse-transition
-          mode="out-in"
-      >
-        <v-icon
-            :key="`icon-${isEditing}`"
-            :color="isEditing ? 'success' : 'info'"
-            @click="isEditing = !isEditing"
-            v-text="isEditing ? 'mdi-check-outline' : 'mdi-circle-edit-outline'"
-        />
-      </v-slide-x-reverse-transition>
-    </template>
-  </v-combobox>
-  <v-row>
-    <v-col cols="9">{{$t("icon_picker.preview")}} <v-icon>{{icon}}</v-icon></v-col>
-    <v-col cols="3" align="end">
-      <v-btn
-          style="margin-left: auto;"
-          @click="saveIcon(icon)">{{$t("buttons.save")}}</v-btn>
-    </v-col>
-  </v-row>
-      <!-- <v-card style="background: whitesmoke">
         <v-row>
           <v-col cols="5" align="center">
             <v-text-field
             v-model="t"
             @input="test()"></v-text-field>
           </v-col>
-          <v-col cols="5" align="center">
+          <v-col cols="7" align="center">
             
-              <v-chip v-for="icon in filteredIcons.value" :key="icon">
+              <v-chip v-for="icon in filteredIcons" :key="icon" @click="changeIcon(icon);">
                 <v-icon :icon="icon"/>
               </v-chip>
 
           </v-col>
-          <v-col cols="2" align="center">
-            <v-btn>Save</v-btn>
-          </v-col>
         </v-row>
-      </v-card> -->
-</v-card>
+      </v-card>
 </template>
 
 <script setup lang="ts">
-import {defineProps, onBeforeMount, ref} from "vue";
+import {defineProps, onBeforeMount, ref, Ref} from "vue";
 import {Icon, Module, Tag} from "@/helpers/types";
 import ModuleService from "@/services/ModuleService";
 import {useRoute, useRouter} from "vue-router";
 import IconService from "@/services/IconService";
 import {AxiosResponse} from "axios";
 import TagService from "@/services/TagService";
-    //import mdi_icons from "@/helpers/mdi-icons.json"
+    import mdi_icons from "@/helpers/mdi-icons.json"
 
 const router = useRouter();
 const iconNames =
@@ -76,22 +44,57 @@ const props = defineProps<{
   tag?: Tag;
 }>();
 
+    const t = ref("");
+    const allIcons = ref([]) as Ref<[]>;
+    const filteredIcons: Ref<String[]> = ref([]) as Ref<String[]>;
 
-    // const allIcons = ref([]) as Ref<[]>;
-    // const filteredIcons: Ref<String[]> = ref([]) as Ref<String[]>;
+    function test(): void {
+      filteredIcons.value = allIcons.value.map(icon => 'mdi-'+icon.name.toString()).filter(name => name.includes(t.value)).slice(0, 5);
+    }
 
-    // function test(): void {
-    //   filteredIcons.value = allIcons.value.map(icon => 'mdi-'+icon.name.toString()).filter(name => name.includes(t.value)).slice(0, 5);
-    //   console.log(filteredIcons.value)
-    // }
+    function jsonToArray(): void {
+      for(var icon in mdi_icons) {
+        allIcons.value.push(mdi_icons[icon]);
+      }
+      filteredIcons.value = allIcons.value.map(icon => 'mdi-'+icon.name.toString()).slice(0, 5)
+      console.log(filteredIcons.value);
+    }
 
-    // function jsonToArray(): void {
-    //   for(var icon in mdi_icons) {
-    //     allIcons.value.push(mdi_icons[icon]);
-    //   }
-    //   filteredIcons.value = allIcons.value.map(icon => 'mdi-'+icon.name.toString()).slice(0, 5)
-    //   console.log(filteredIcons.value);
-    // }
+    async function changeIcon(icon: any): void {
+      const isFound = ref(false);
+      const foundIcon: Ref<Icon> = ref({}) as Ref<Icon>
+      const newIcon: Icon = {
+        icon_id: 0,
+        reference: icon,
+      };
+      (await IconService.getIcons().then(async res => {
+        res.data.forEach(iconRes => {
+          if(iconRes.reference.toLowerCase() === newIcon.reference.toLowerCase()) {
+            isFound.value = true;
+            foundIcon.value = iconRes;
+          }
+        })
+        if(!isFound.value) {
+          (await IconService.addIcon(newIcon).then(async () => {
+            (await IconService.getIcons().then(res => {
+              res.data.forEach(iconRes => {
+                if(iconRes.reference.toLowerCase() === icon.toLowerCase()) foundIcon.value = iconRes;
+              })
+            }))
+          }));
+        }
+      }))
+      //add icon to module
+      if(module != null) {
+        module.icon = foundIcon.value;
+        ModuleService.editModule(module);
+      }
+      if(tag != null) {
+        tag.icon = foundIcon.value;
+        TagService.editTag(tag);
+      }
+      //ModuleService.editModule
+    }
 
 //returns 0 if it doesn't exist, returns corresponding number if it exists, linear search
 function verifyIconExists(iconName: string){
@@ -149,7 +152,7 @@ function contextBasedSave(icon: Icon){
 
 const route = useRoute();
 onBeforeMount(async ()=>{
-      //jsonToArray();
+      jsonToArray();
   if(route.params.module){
     ModuleService.getModule(
         route.params.module instanceof Array
